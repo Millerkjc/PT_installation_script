3d193a75fdc9d014a951fac20b8a6aad663ee53a
Mill <millerkjc@gmail.com>
2018-02-22 10:56:05 +0100

    Fixed directory

--- InstallPT.sh
+++ InstallPT.sh
@@ -3,6 +3,16 @@
 BASE_FOLDER=~/Downloads
 PT_FOLDER=PT_tar
 TARGET_FOLDER="$BASE_FOLDER/$PT_FOLDER"
+ERROR_STRING="Run 'sudo rm -rf ~/Downloads/PT_tar/' and run the script again"
+
+# FUNCTIONS
+pushd(){
+    TEMP_FOLDER=$@
+    PUSHED_FOLDER=${TEMP_FOLDER#$HOME}
+    echo "cd from $DIRSTACK to $DIRSTACK$PUSHED_FOLDER"
+    command pushd "$@" > /dev/null
+}
+
 
 if [ "$EUID" -ne 0 ]; then
   echo "Please run as root"
@@ -10,14 +20,15 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 if [ -d "$TARGET_FOLDER" ]; then
-  echo "Folder exists. Run 'sudo rm -rf ~/Downloads/PT_tar/' and run the script again"
+  echo -n "Folder exists. $ERROR_STRING"
   exit
 fi
 
 echo "Creating dir to unpack PT tar"
-mkdir "$TARGET_FOLDER"
+pushd $BASE_FOLDER
+mkdir $PT_FOLDER
 echo "Starting unpacking PT"
-tar -xf ~/Downloads/Packet\ Tracer\ 7.1.1\ for\ Linux\ 64\ bit.tar -C "$TARGET_FOLDER" 2> /dev/null
+tar -xf ~/Downloads/Packet\ Tracer\ 7.1.1\ for\ Linux\ 64\ bit.tar -C "$PT_FOLDER" 2> /dev/null
 if [[ $? -ne 0 ]]
 then
     echo "Something went wrong during untar pkg"
@@ -28,9 +39,16 @@ else
 fi
 
 echo "Start running ~/Downloads/PT_tar/install"
-$TARGET_FOLDER/install
-echo $?
+./install
+if [ $? -ne 0 ]; then
+    echo "Problem during Cisco PT installation script"
+fi
 echo "Finish running ~/Downloads/PT_tar/install"
 
 
+if [ -d "/opt/pt" ]; then
+  echo TODO 
+  exit
+fi
 
+# Should we create a symbolic link "packettracer" in /usr/local/bin for easy Cisco Packet Tracer startup? [Yn]
