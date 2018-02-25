4be99704352f2fcb89d615e172afaacdca42344d
Mill <millerkjc@gmail.com>
2018-02-25 19:24:51 +0100

    Fixed directory 2, functions added

--- InstallPT.sh
+++ InstallPT.sh
@@ -1,16 +1,29 @@
 #!/bin/bash
 
-BASE_FOLDER=~/Downloads
-PT_FOLDER=PT_tar
-TARGET_FOLDER="$BASE_FOLDER/$PT_FOLDER"
+#
+# Prerequisiti -> apt-file
+#
+
+
+BASE_FOLDER=/home/$SUDO_USER/Downloads/
+PT_TMP=PT_tar
+TARGET_FOLDER="$BASE_FOLDER$PT_TMP"
+PT_FOLDER=/opt/pt
+
+# STRINGS
 ERROR_STRING="Run 'sudo rm -rf ~/Downloads/PT_tar/' and run the script again"
 
-# FUNCTIONS
-pushd(){
-    TEMP_FOLDER=$@
-    PUSHED_FOLDER=${TEMP_FOLDER#$HOME}
-    echo "cd from $DIRSTACK to $DIRSTACK$PUSHED_FOLDER"
+# DEFINE FUNCTION
+pushd () {
+#    Strings for debugging
+#    TEMP_FOLDER=$@
+#    PUSHED_FOLDER=${TEMP_FOLDER#$HOME}
+#    echo "cd from $DIRSTACK to $PUSHED_FOLDER"
     command pushd "$@" > /dev/null
+}
+
+popd () {
+    command popd "$@" > /dev/null
 }
 
 
@@ -20,35 +33,43 @@ if [ "$EUID" -ne 0 ]; then
 fi
 
 if [ -d "$TARGET_FOLDER" ]; then
-  echo -n "Folder exists. $ERROR_STRING"
+  echo $TARGET_FOLDER
+  echo "Folder exists. $ERROR_STRING"
   exit
 fi
 
-echo "Creating dir to unpack PT tar"
-pushd $BASE_FOLDER
-mkdir $PT_FOLDER
+#echo "Creating dir to unpack PT tar"
+pushd "$BASE_FOLDER"
+#cd $BASE_FOLDER
+
+mkdir "$PT_TMP"
 echo "Starting unpacking PT"
-tar -xf ~/Downloads/Packet\ Tracer\ 7.1.1\ for\ Linux\ 64\ bit.tar -C "$PT_FOLDER" 2> /dev/null
+pwd
+tar -xf $BASE_FOLDER/Packet\ Tracer\ 7.1.1\ for\ Linux\ 64\ bit.tar -C "$PT_TMP" 2> /dev/null
 if [[ $? -ne 0 ]]
 then
     echo "Something went wrong during untar pkg"
-    echo "run 'sudo rm -rf ~/Downloads/PT_tar/' and run the script again"
+    echo "$ERROR_STRING"
     exit 1
 else
     echo "Finish unpacking PT - Job complete without error"
 fi
 
+pushd "$PT_TMP"
+#cd $PT_TMP
 echo "Start running ~/Downloads/PT_tar/install"
 ./install
-if [ $? -ne 0 ]; then
-    echo "Problem during Cisco PT installation script"
-fi
 echo "Finish running ~/Downloads/PT_tar/install"
 
 
-if [ -d "/opt/pt" ]; then
-  echo TODO 
-  exit
+if [ -d "$PT_FOLDER" ]; then
+  echo "PT copied in $PT_FOLDER"
+  echo "Start removing: $PT_TMP"
+  popd
+  rm -rf $PT_TMP
+  echo "Finish removing: $PT_TMP"
 fi
 
-# Should we create a symbolic link "packettracer" in /usr/local/bin for easy Cisco Packet Tracer startup? [Yn]
+# RETURN TO THE STARTER DIR
+popd
+rm -rf $TARGET_FOLDER
